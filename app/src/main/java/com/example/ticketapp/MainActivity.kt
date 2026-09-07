fce20f39feda21a49243d61aaa36990d8a787225
TheMisterKyle <mistermcgiverin@gmail.com>
2026-09-07 12:41:12 -0400

    Remove reintroduced pointer import

--- app/src/main/java/com/example/ticketapp/MainActivity.kt
+++ app/src/main/java/com/example/ticketapp/MainActivity.kt
@@ -42,7 +42,6 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
